fd296d52b66f8a154753f6d5a3a74d5c3ccd509c
Yodelyoda <47818799+Yodelyoda@users.noreply.github.com>
2022-04-06 14:42:58 +0300

    Add files via upload

---
+++ 2lab 4.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2lab_4._1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Vector v = new Vector();
+            v.Read();
+            v.Display();
+            Console.WriteLine("Area: " + v.GetArea());
+            Console.WriteLine("Perim: " + v.GetPerim());
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 2lab 4.1/Vector.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2lab_4._1
+{
+    class Vector
+    {
+        private double hight;
+        private double width;
+       
+        public double GetHight() { return hight; }
+        public double GetWidth() { return width; }
+
+        public double GetArea() { return hight * width; }
+
+        public double GetPerim() { return (hight + width)*2; }
+
+        public bool SetHight(double h)
+        {
+            if (h > 0)
+            {
+                hight = h;
+                return true;
+            }
+            else
+            {
+                hight = 0;
+                return false;
+            }
+        }
+        public bool SetWidth(double w)
+        {
+            if (w > 0)
+            {
+                width = w;
+                return true;
+            }
+            else
+            {
+                width = 0;
+                return false;
+            }
+        }
+     
+        public bool Init(double h, double w)
+        {
+            return SetHight (h) && SetWidth(w);
+        }
+        public void Read()
+        {
+         
+            double h;
+            double w;
+          
+            do
+            {
+                Console.WriteLine("Hight: ");
+                h = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Width: ");
+                w = Convert.ToDouble(Console.ReadLine());
+            } while (!Init(h, w));
+        }
+        public void Display()
+        {
+         
+            Console.WriteLine("Hight: " + hight);
+            Console.WriteLine("Width: " + width);
+        }
+
+    }
+}
